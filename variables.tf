9241e3ab084063c8121607447f015a9bf3ceec42
Meghashree N <meghashreetendulkar@gmail.com>
2023-04-20 20:01:01 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,3 +1,9 @@
+variable "tenancy_ocid" {}
+variable "user_ocid" {}
+variable "fingerprint" {}
+variable "private_key_path" {}
+variable "compartment_ocid" {}
+
 variable "parent_compartment" {
   description = "parent compartment id under which this compartment is created"
   type        = string
