fbddb47fd57ecc42d7f2e77d34c3b098342cb6d5
Meghashree N <meghashreetendulkar@gmail.com>
2023-04-20 19:49:07 +0530

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,7 @@
+resource "oci_identity_compartment" "asset_compartment" {
+    compartment_id = "${var.parent_compartment}"
+    name = "Asset_mgmt_solutions"
+    description = "compartment for Retail related resources"
+    enable_delete = "true"
+    
+}
