1c218a49e041aa85dc07b4ee60c88654fec9b3fe
Meghashree N <meghashreetendulkar@gmail.com>
2023-04-20 22:13:16 +0530

    Update linux_instance.tf

--- linux_instance.tf
+++ linux_instance.tf
@@ -1,24 +1,24 @@
 resource "oci_core_instance" "linux" {
   count               = "1"
- availability_domain = "${lookup(data.oci_identity_availability_domains.ads.availability_domains[0],"name")}"
- compartment_id      = "${var.compartment_ocid}"
-  display_name        = "${var.instance_name}"
-  shape               = "${var.shape}"
+ availability_domain = lookup(data.oci_identity_availability_domains.ads.availability_domains[0],"name")
+ compartment_id      = oci_identity_compartment.asset_compartment.id
+  display_name        = var.instance_name
+  shape               = var.shape
 
   create_vnic_details {
-    assign_public_ip = "${var.assign_public_ip}"
+    assign_public_ip = var.assign_public_ip
     display_name   = "primaryvnic"
     hostname_label = "linuxInstance"
-    subnet_id      = "${oci_core_subnet.publicsubnet.id}"
+    subnet_id      = oci_core_subnet.publicsubnet.id
   }
 
   metadata = {
-    ssh_authorized_keys = "${file("${var.ssh_authorized_keys}")}"
+    ssh_authorized_keys = file(var.ssh_authorized_keys)
   }
 
   source_details {
-    boot_volume_size_in_gbs = "${var.boot_volume_size_in_gbs}"
-    source_id               = "${var.instance_image_ocid[var.region]}"
+    boot_volume_size_in_gbs = var.boot_volume_size_in_gbs
+    source_id               = var.instance_image_ocid[var.region]
     source_type             = "image"
   }
   timeouts {
@@ -27,17 +27,17 @@ resource "oci_core_instance" "linux" {
 }
 
 resource "oci_core_volume" "linux" {
-  availability_domain = "${lookup(data.oci_identity_availability_domains.ads.availability_domains[0],"name")}"
-  compartment_id      ="${var.compartment_ocid}"
-  display_name        = "Terraform_deployed_Instance"
-  size_in_gbs         = "${var.boot_volume_size_in_gbs}"
+  availability_domain = lookup(data.oci_identity_availability_domains.ads.availability_domains[0],"name")
+  compartment_id      = oci_identity_compartment.asset_compartment.id
+  display_name        = "asset_mgmt_Instance"
+  size_in_gbs         = var.boot_volume_size_in_gbs
 }
 
 resource "oci_core_volume_attachment" "linux" {
   count           = "1"
   attachment_type = "iscsi"
-  compartment_id  = "${var.compartment_ocid}"
-  instance_id     = "${oci_core_instance.linux[count.index].id}"
-  volume_id       = "${oci_core_volume.linux.id}"
+  compartment_id  = oci_identity_compartment.asset_compartment.id
+  instance_id     = oci_core_instance.linux[count.index].id
+  volume_id       = oci_core_volume.linux.id
   use_chap        = true
 }
