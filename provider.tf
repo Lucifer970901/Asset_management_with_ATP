5c5afacec08d3d1629e74ccc9d481f09e508ea56
Meghashree N <meghashreetendulkar@gmail.com>
2023-04-20 20:13:42 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -2,7 +2,7 @@ provider "oci" {
   version          = ">=3.11"
   tenancy_ocid     = var.tenancy_ocid
   user_ocid        = var.user_ocid
-  fingerprint      = var.api_fingerprint
-  private_key_path = var.api_private_key_path
+  fingerprint      = var.fingerprint
+  private_key_path = var.private_key_path
   region           = var.region
 }
