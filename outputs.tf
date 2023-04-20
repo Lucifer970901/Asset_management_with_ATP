74b9dbbfb52ddf3996a0360c2ac3c85c62998b10
Meghashree N <meghashreetendulkar@gmail.com>
2023-04-20 21:40:44 +0530

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -2,3 +2,7 @@ output "asset_compartment" {
   description = "The VCNs, indexed by display_name."
   value       = oci_identity_compartment.asset_compartment.name
 }
+
+output "vcn_id" {
+value = oci_core_vcn.test_vcn.id
+}
