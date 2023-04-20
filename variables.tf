266cabca03e300fa4b12bb3b180ca486781a838c
Meghashree N <meghashreetendulkar@gmail.com>
2023-04-20 22:00:12 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -53,3 +53,37 @@ description = "note that the cidr block for the subnet must be smaller and part 
 default     = "10.0.1.0/24"
 }
 
+#variables to create linux instance
+variable instance_image_ocid {
+  type = "map"
+
+  default = {
+    # Updated to Oracle Linux 7.8 with all patches as of April 2020.
+
+    ap-sydney-1 = "ocid1.image.oc1.ap-sydney-1.aaaaaaaao3vineoljixw657cxmbemwasdgirfy6yfgaljqsvy2dq7wzj2l4q"
+    us-ashburn-1 = "ocid1.image.oc1.iad.aaaaaaaahjkmmew2pjrcpylaf6zdddtom6xjnazwptervti35keqd4fdylca"
+    us-phoenix-1 = "ocid1.image.oc1.phx.aaaaaaaav3isrmykdh6r3dwicrdgpmfdv3fb3jydgh4zqpgm6yr5x3somuza"
+    ap-mumbai-1  = "ocid1.image.oc1.ap-mumbai-1.aaaaaaaanwfcq3baulkm73kcimzymx7qgfpoja2b56wgwhopjjgrz4om67zq"
+  }
+}
+
+variable "instance_name"{
+description  = "provide the display name for thelinux instance to be deployed"
+}
+
+variable "ssh_authorized_keys" {
+  default = "C:/Users/megn/new.pub"
+}
+
+variable "preserve_boot_volume" {
+  default = false
+}
+variable "boot_volume_size_in_gbs" {
+  default = "50"
+}
+variable "shape" {
+  default = "VM.Standard2.1"
+}
+variable "assign_public_ip" {
+  default = true
+}
