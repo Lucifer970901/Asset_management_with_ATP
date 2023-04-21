6ea9aedb7bacef97f0b3c042df0568ff9da7329e
Meghashree N <meghashreetendulkar@gmail.com>
2023-04-21 17:47:28 +0530

    Update oic.tf

--- oic.tf
+++ oic.tf
@@ -2,7 +2,7 @@ resource "oci_integration_integration_instance" "test_integration_instance" {
     #Required
     compartment_id = oci_identity_compartment.asset_compartment.id
     display_name = var.integration_instance_display_name
-    integration_instance_type = var.integration_instance_integration_instance_type
+    integration_instance_type = "Enterprise"
     is_byol = var.integration_instance_is_byol
     message_packs = var.integration_instance_message_packs
 }
