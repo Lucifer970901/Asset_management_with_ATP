547b0ad3826ad6bc026082a23e99368efbed6e69
Meghashree N <meghashreetendulkar@gmail.com>
2023-04-23 17:19:16 +0530

    Update oic.tf

--- oic.tf
+++ oic.tf
@@ -5,5 +5,5 @@ resource "oci_integration_integration_instance" "test_integration_instance" {
     integration_instance_type = var.integration_instance_integration_instance_type
     is_byol = var.integration_instance_is_byol
     message_packs = var.integration_instance_message_packs
-   # idcs_at = file(tokens.tok)
+    idcs_at = file(tokens.tok)
 }
